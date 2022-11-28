67cbadb6badceff2ff69ebb7560690c29308e24c
Mark <24markmark@gmail.com>
2022-11-28 22:11:23 +0300

    LauncherActivity(переходы с сохранением состояния)

--- app/src/main/java/com/example/learn1/MainActivity.kt
+++ app/src/main/java/com/example/learn1/MainActivity.kt
@@ -1,65 +1,35 @@
 package com.example.learn1
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import com.example.learn1.constant.Password
-import com.example.learn1.constant.Post
-import com.example.learn1.constant.Salary
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.learn1.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var AMBСClass : ActivityMainBinding
+    private var launcher: ActivityResultLauncher<Intent>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AMBСClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(AMBСClass.root)
-        AMBСClass.printButton.setOnClickListener(){
-            AMBСClass.payText.visibility = View.VISIBLE
-            AMBСClass.workerphoto.visibility = View.VISIBLE
-            val name = AMBСClass.writeName.text.toString()
-            when(name){
-                Post.DIRECTOR -> {
-                    if (AMBСClass.passwordwrite.text.toString().toInt() == Password.DIRECTOR_PASSWORD){
-                        val text = "Вот ваши ${Salary.DIRECTOR_SALARY}$ получите и распишитесь"
-                        AMBСClass.payText.text =  text
-                        AMBСClass.workerphoto.setImageResource(R.drawable.face)
-                    }
-                    else{
-                        AMBСClass.payText.text =  "Неверный пароль"
-                        AMBСClass.workerphoto.setImageResource(R.drawable.error)
-                    }
-                }
-
-                Post.DEVELOPER -> {
-                    if (AMBСClass.passwordwrite.text.toString().toInt() == Password.DEVELOPER_PASSWORD){
-                        val text = "Вот ваши ${Salary.DEVELOPER_SALARY}$ получите и распишитесь"
-                        AMBСClass.payText.text =  text
-                        AMBСClass.workerphoto.setImageResource(R.drawable.face_1) }
-                    else{
-                        AMBСClass.payText.text =  "Неверный пароль"
-                        AMBСClass.workerphoto.setImageResource(R.drawable.error)
-                    }
-                }
-
-                Post.MANAGER -> {
-                    if (AMBСClass.passwordwrite.text.toString().toInt() == Password.MANAGER_PASSWORD){
-                        val text = "Вот ваши ${Salary.MANAGER_SALARY}$ получите и распишитесь"
-                        AMBСClass.payText.text =  text
-                        AMBСClass.workerphoto.setImageResource(R.drawable.face_2)
-                    }
-                    else{
-                        AMBСClass.payText.text =  "Неверный пароль"
-                        AMBСClass.workerphoto.setImageResource(R.drawable.error)
-                    }
-                }
-
-              else -> {
-                  AMBСClass.payText.text = "Такого работника нет иди отсюда"
-              }
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result: ActivityResult ->
+            if (result.resultCode == RESULT_OK){
+                val test = result.data?.getStringExtra("count_letter")
+                AMBСClass.textView5.text = result.data?.getStringExtra("count_letter")
             }
         }
     }
+
+    fun onClickone(view: View){
+        launcher?.launch(Intent(this, MainActivity2::class.java))
+    }
+
 }
